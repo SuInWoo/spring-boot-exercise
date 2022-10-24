f87a8be9ebeb7f650b9de186ca06ad488e62114c
SuInWoo <wbo1026@naver.com>
2022-10-24 13:18:33 +0900

    datasource 적용

--- src/main/java/com/dao/LocalConnectionMaker.java
+++ src/main/java/com/dao/LocalConnectionMaker.java
@@ -1,18 +1,18 @@
-//package com.dao;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//import java.util.Map;
-//
-//public class LocalConnectionMaker implements ConnectionMaker{
-//
-//    @Override
-//    public Connection getConnection() throws SQLException {
-//
-//        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/likelion-db",
-//                "root", "root");
-//
-//        return conn;
-//    }
-//}
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class LocalConnectionMaker implements ConnectionMaker{
+
+    @Override
+    public Connection getConnection() throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/likelion-db",
+                "root", "root");
+
+        return conn;
+    }
+}
